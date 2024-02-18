c00a75a32ccdf798d66ce9711a83f4f7984654fb
Rendy Pratama <rendypratama752@gmail.com>
2024-02-19 00:36:18 +0700

    ADD: offline capability with workbox

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,5 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev',
-  CACHE_NAME: 'RestaurantApp',
   DATABASE_NAME: 'restaurant-app-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurant',
--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,4 @@
+/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -39,6 +40,26 @@ module.exports = {
     }),
     new WorkboxWebpackPlugin.GenerateSW({
       swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) =>
+            url.href.startsWith(
+              'https://restaurant-api.dicoding.dev/images/medium/',
+            ),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'restaurantapp-image-api',
+          },
+        },
+        {
+          urlPattern: ({ url }) =>
+            url.href.startsWith('https://restaurant-api.dicoding.dev/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'restaurantapp-api',
+          },
+        },
+      ],
     }),
   ],
 };
